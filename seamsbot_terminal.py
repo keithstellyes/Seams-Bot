20f58efabef42908f19a54d13d5568ae7e5f56a9
Keith Stellyes <keithastellyes@gmail.com>
2016-02-27 21:09:45 -0800

    Revert "Initial Commit"
    This reverts commit efb1cb902027459ac818beb10da6d343d52c0aae.

--- seamsbot_terminal.py
+++
@@ -1,6 +0,0 @@
-from commandlookup import processEval
-
-inputStr = ""
-while not (inputStr.lower().startswith("quit") or inputStr.lower().startswith("exit")):    
-    inputStr = input("Input:")
-    print(processEval(inputStr))
