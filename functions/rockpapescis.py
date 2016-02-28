ca0679737933608007e3e30809ad9aacc3017e81
Keith Stellyes <keithastellyes@gmail.com>
2016-02-28 15:41:38 -0800

    Trailing whitespace glitch in rps fixed

--- functions/rockpapescis.py
+++ functions/rockpapescis.py
@@ -42,10 +42,15 @@ def rpsGame(args):
         playerB = mode[2]
         playerBMove = mode[4]
 
+        print("AMove",playerAMove,"BMove",playerBMove)
+
         if playerA == recipient:
             playerAMove = args[1]
         elif playerB == recipient:
             playerBMove = args[1]
+
+        playerAMove = playerAMove.strip()
+        playerBMove = playerBMove.strip()
 
         if playerAMove.lower() not in validMoves or playerBMove.lower() not in validMoves:
             lines[index] = mode[0]+";"+mode[1]+";"+mode[2]+";"+playerAMove+";"+playerBMove+"\n"
@@ -88,8 +93,8 @@ def rpsGame(args):
         for line in lines:
             file.write(line+"\n")
         file.close()
-        sendMessage(message="You have been challenged to a game of Rock/Paper/Scissors! To make your move, type exactly, replacing rock with paper or scissors if desired: rps "+gameName+" rock",recipientOfSendMessage=args[1],mode="sms")
-        return "Challenge sent. TO make your move text exactly, replacing rock with your move rps "+gameName+"rock"+args[1]
+        sendMessage(message="You have been challenged to a game of Rock/Paper/Scissors! To make your move, type exactly, replacing rock with paper or scissors: rps "+gameName+" rock",recipientOfSendMessage=args[1],mode="sms")
+        return "Challenge sent. Game name: "+gameName
                                                
 ##        file = open("save.txt","w")
 ##        file.write(
