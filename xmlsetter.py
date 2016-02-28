61d1fe31d48e0e6b16b3c42468c4bd60cef9277a
Keith Stellyes <keithastellyes@gmail.com>
2016-02-27 21:09:45 -0800

    Revert "Initial Commit"
    This reverts commit db60ee7aea1b563b5862a57ecdfb7a6c4d1694a9.

--- xmlsetter.py
+++
@@ -1,9 +0,0 @@
-___name___ = "xmlsetter"
-
-import xml.etree.ElementTree as ET
-
-def returnSetting(settingName):
-    ___name___ = "returnSetting"
-    tree = ET.parse("settings/settings.xml")
-    root = tree.getroot()
-    return root.find(settingName).text
