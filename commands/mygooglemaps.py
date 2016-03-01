bda239862d07a95b13212cdcc3cb6c3ea7d007b5
Keith Stellyes <keithastellyes@gmail.com>
2016-02-29 16:20:30 -0800

    "Functions" -> "Commands"
    Renaming functions as commands for clarity

---
+++ commands/mygooglemaps.py
@@ -0,0 +1,35 @@
+import googlemaps
+from datetime import *
+import json
+from xmlsetter import returnSetting
+
+
+___name___ = "mygooglemaps"
+gmaps = googlemaps.Client(key=returnSetting("googleapikey"))
+
+#gmaps mode origin::destination
+
+def getGMapsDirections(args):
+    ___name___ = "getGMapsDirections"
+    
+    def directions(origin,destination,methodOfTravel):
+        now = datetime.now()
+        ##directions_result = gmaps.directions("Sydney Town Hall",
+        ##                                     "Parramatta, NSW",
+        ##                                     mode="driving",
+        ##                                     departure_time=now)
+        directions_result = gmaps.directions(origin,
+                                             destination,
+                                             mode=methodOfTravel,
+                                             departure_time = now)
+        returnString = ""
+        for v in directions_result[0]['legs'][0]['steps']:
+            returnString+="["+str(v['html_instructions'])+"]"
+        returnString = returnString.replace("<b>","")
+        returnString = returnString.replace("</b>","")
+        returnString = returnString.replace('<div style="font-size:0.9em">',"][")
+        return returnString.replace("</div>","")
+    
+    routeArgs = args[1].split("::")
+    return directions(routeArgs[0],routeArgs[1],args[0])
+    
